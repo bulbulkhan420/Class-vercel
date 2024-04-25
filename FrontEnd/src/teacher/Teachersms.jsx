fe2694a6e9ca7a57eee92b2665df2b0669e344a5
Md sha alam bulbul <79450874+bulbulkhan420@users.noreply.github.com>
2024-04-25 20:20:53 +0600

    Update Teachersms.jsx

--- FrontEnd/src/teacher/Teachersms.jsx
+++ FrontEnd/src/teacher/Teachersms.jsx
@@ -6,7 +6,7 @@ import teci from './teacher.module.css'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import io from 'socket.io-client'
-let socket=io.connect("https://class-vercel-backend.vercel.app");
+let socket=io.connect("https://renderbackendbbb.onrender.com");
 export default function Teachersms() {
     let {id}=useParams();
     let [tin,stin]=useState({});
@@ -39,7 +39,7 @@ export default function Teachersms() {
       
     }
     useEffect(()=>{
-      axios.post("https://class-vercel-backend.vercel.app/teacherinfo",{
+      axios.post("https://renderbackendbbb.onrender.com/teacherinfo",{
         id
       })
       .then((res)=>{
